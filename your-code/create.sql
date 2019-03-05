4da82ebedbaa4ca1c2cf4c63f45dacd8ea9631a4
jbe1992 <46814329+jbe1992@users.noreply.github.com>
2019-03-05 17:30:06 +0100

    Update create.sql

--- your-code/create.sql
+++ your-code/create.sql
@@ -39,6 +39,7 @@ CREATE TABLE `Invoices` (
   CONSTRAINT `invoices_ibfk_3` FOREIGN KEY (`Sales Person`) REFERENCES `salespersons` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+CREATE TABLE `Salespersons` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `Staff ID` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
   `Name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
